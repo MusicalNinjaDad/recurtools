36ac115e9627a2f86f98a3457a1de6c9c457efc1
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-28 19:54:50 +0100

    magic number comparisons are OK in tests

--- tests/test_len.py
+++ tests/test_len.py
@@ -5,10 +5,10 @@ from recurtools import nested
 # https://stackoverflow.com/questions/72321903/deep-list-count-count-lists-within-lists
 
 def test_len_ints():
-    assert len(nested([1, 2, [3, 4]])) == 4  # noqa: PLR2004
+    assert len(nested([1, 2, [3, 4]])) == 4
 
 def test_len_strs():
-    assert len(nested(["a", "b", ["c", "d", ["e"]]])) == 5  # noqa: PLR2004
+    assert len(nested(["a", "b", ["c", "d", ["e"]]])) == 5
 
 def test_len_emptylists():
     assert len(nested([[[]]])) == 0
@@ -17,10 +17,10 @@ def test_len_docstring():
     assert len(nested(6)) == 1
 
 def test_len_numpyarray():
-    assert len(nested(array([[1,2],[3,4]]))) == 4  # noqa: PLR2004
+    assert len(nested(array([[1,2],[3,4]]))) == 4
 
 def test_len_countcollections():
-    assert len(nested([1, 2, [3, 4]], countcontainers=True)) == 5  # noqa: PLR2004
+    assert len(nested([1, 2, [3, 4]], countcontainers=True)) == 5
 
 def test_len_singleint_countcollections():
     assert len(nested(6, True)) == 0  # noqa: FBT003
