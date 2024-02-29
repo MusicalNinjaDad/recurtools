1e1179df7782df408d4dd2f2616c41524920646c
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-29 11:21:57 +0100

    RED: update the tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,26 +1,26 @@
 from pytest import raises  # noqa: PT013
 
-from recurtools import indexrecursive
+from recurtools import nested
 from recurtools.utils import NotFoundError
 
 # https://stackoverflow.com/questions/72321903/deep-list-count-count-lists-within-lists
 
 def test_index_toplevel():
-    assert indexrecursive([1, 2, [3, 2]],2) == (1,)
+    assert nested([1, 2, [3, 2]]).index(2) == (1,)
 
 def test_index_int():
-    assert indexrecursive([1, 2, [3, 2]],3) == (2,0)
+    assert nested([1, 2, [3, 2]]).index(3) == (2,0)
 
 def test_index_notfound():
     with raises(NotFoundError):
-        indexrecursive([1, 2, [3, 2]],4)
+        nested([1, 2, [3, 2]]).index(4)
 
 def test_index_later():
-    assert indexrecursive([1, 2, [3, 2],[[2,3],[5,5,3,4]]],4) == (3,1,3)
+    assert nested([1, 2, [3, 2],[[2,3],[5,5,3,4]]]).index(4) == (3,1,3)
 
 def test_string():
-    assert indexrecursive("FooBar","B") == (3,)
-    assert indexrecursive(["Foo",[1,"Bar"]],"a") == (1,1,1)
+    assert nested("FooBar").index("B") == (3,)
+    assert nested(["Foo",[1,"Bar"]]).index("a") == (1,1,1)
 
 def test_set():
-    assert indexrecursive([1,2,{3,4},[3,4]],4) == (3,1)
+    assert nested([1,2,{3,4},[3,4]]).index(4) == (3,1)
