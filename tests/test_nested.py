4557aa05fb2fb401dedc93b92af11d0facfd0fe2
Mike Foster <i.am.mike.foster@gmail.com>
2024-02-29 06:55:56 +0100

    fixup: magicnumber comparison in tests

--- tests/test_nested.py
+++ tests/test_nested.py
@@ -9,18 +9,18 @@ def test_storescollection():
 def test_len():
     input = [1,2,[3,4,[5],6],7,[8,9]]  # noqa: A001
     nest = nested(input)
-    assert len(nest) == 9  # noqa: PLR2004
+    assert len(nest) == 9
 
 def test_in():
     input = [1,2,[3,4,[5],6],7,[8,9]]  # noqa: A001
     nest = nested(input)
-    assert 4 in nest  # noqa: PLR2004
+    assert 4 in nest
     assert 0 not in nest
 
 def test_count():
     input = [1,2,[3,2,[5],6],7,[2,9]]  # noqa: A001
     nest = nested(input)
-    assert nest.count(2) == 3  # noqa: PLR2004
+    assert nest.count(2) == 3
 
 def test_iterate():
     input = [1,2,[3,4,[5],6],7,[8,9]]  # noqa: A001
--- tests/test_sum.py
+++ tests/test_sum.py
@@ -5,7 +5,7 @@ from recurtools import sumrecursive
 # https://stackoverflow.com/questions/72321903/deep-list-count-count-lists-within-lists
 
 def test_sum_ints():
-    assert sumrecursive([1, 2, [3, 4]]) == 10  # noqa: PLR2004
+    assert sumrecursive([1, 2, [3, 4]]) == 10
 
 def test_sum_strs():
     assert sumrecursive(["a", "b", ["c", "d", ["e"]]]) == 0
@@ -14,10 +14,10 @@ def test_sum_emptylists():
     assert sumrecursive([[[]]]) == 0
 
 def test_sum_docstring():
-    assert sumrecursive(6) == 6  # noqa: PLR2004
+    assert sumrecursive(6) == 6
 
 def test_sum_numpyarray():
-    assert sumrecursive(array([[1,2],[3,4]])) == 10  # noqa: PLR2004
+    assert sumrecursive(array([[1,2],[3,4]])) == 10
 
 def test_sum_mixed():
-    assert sumrecursive([1,2.5,[4,"foo"],(5,(0.5,5))]) == 18  # noqa: PLR2004
+    assert sumrecursive([1,2.5,[4,"foo"],(5,(0.5,5))]) == 18
