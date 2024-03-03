927d9ab1223a8cb775fef853b1031ad617fa4c09
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-03 19:28:12 +0100

    RED: add flatten method

--- tests/test_contents.py
+++ tests/test_contents.py
@@ -1,10 +1,21 @@
+import pytest
+
 from recurtools import nested
 
 
-def test_preservestrings():
+def test_list_nest():
     nest = nested([
         [1,2],
         [3,4],
         ["abc","def"],
     ])
     assert list(nest) == [1,2,3,4,"abc","def"]
+
+@pytest.mark.xfail(reason="Not Implemented")
+def test_flatten_default():
+    nest = nested([
+        [1,2],
+        [3,4],
+        ["abc","def"],
+    ])
+    assert list(nest.flatten()) == [1,2,3,4,"abc","def"]
